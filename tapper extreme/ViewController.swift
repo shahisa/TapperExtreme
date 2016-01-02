b98a2d77bf12392496913b277a9084769937dff1
John Doe <iswea1@morgan.edu>
2016-01-02 02:27:31 -0500

    Add funcs for hiding elements and starting the game

--- tapper extreme/ViewController.swift
+++ tapper extreme/ViewController.swift
@@ -9,17 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    //Properties
+    var maxTaps = 0
+    var currentTaps = 0
+    
+    
+    //Outlets
+    @IBOutlet weak var tapperImg: UIImageView!
+    @IBOutlet weak var tapsTxt: UITextField!
+    @IBOutlet weak var coinButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var tapsLabel: UILabel!
+    
+    @IBAction func onCoinTapped (sender:UIButton!){
+        currentTaps++
+        updateTapsLabel()
+        
+        if isGameOver() {
+            restartGame()
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onPlayBtnPressed (sender:UIButton!) {
+        hiddenAreas()
+        
+        if tapsTxt.text != nil && tapsTxt.text != "" {
+            
+            
+            maxTaps = Int(tapsTxt.text!)!
+            currentTaps = 0
+            
+            
+        }
+        
+       
+        
     }
-
-
+    func hiddenAreas (){
+        
+        tapsTxt.hidden = true
+        tapperImg.hidden = true
+        playButton.hidden = true
+        
+        coinButton.hidden = false
+        tapsLabel.hidden = false
+    }
+    
+    func restartGame(){
+        maxTaps = 0
+        tapsTxt.text = ""
+        hiddenAreas()
+        
+    }
+    
+    func isGameOver() -> Bool{
+        if currentTaps >= maxTaps {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    
+    func updateTapsLabel (){
+        tapsLabel.text = "\(currentTaps) Taps"
+    }
 }
 
